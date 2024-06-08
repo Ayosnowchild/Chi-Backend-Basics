f3856b3d017c89464a8c7e9dd735c803bcd7c46d
Ayokunle Adekoya <adekoyaasap@gmail.com>
2024-06-08 09:08:44 +0100

    commit message

---
+++ JS class/Intro/script.js
@@ -0,0 +1,51 @@
+// outputing data from javacript with alert
+// alert("hello world");
+
+//  outputing data to the console
+// console.log("this should go to the console");
+
+// interacting with the user with prompt
+// let human = prompt("What is your name?", "");
+// alert(`hello there ${human}`);
+
+// Javascript syntax: variable decaration and assignment
+// let name = "Joe";
+// let name; //variable declaration
+// name = 'Joe' //variable assignment
+// javascript statement
+let x = 2;
+let y = 4;
+let z = x + y;
+console.log(`the value of z is ${z}`);
+// 16-19 javascript programme
+
+// let, const, var
+
+name = "ayo";
+let Name = "joe"; // locally scoped, cannot redclare
+var myName = "helen"; // globally scoped,
+const carName = "Mazda"; // when you are naming a constant, locally scoped cannot redclare or reassign
+
+// var myName = "anna"
+// let Name = "John"
+
+//String
+let name = `String`; //single quote, double quote or backticks
+//Number
+let num = 10;
+// Bigint
+let bigInt = 239039347847487494984940398038;
+// Boolean
+let bool = true;
+// Undefined
+let und;
+// Null
+let nill = null;
+// Symbol or operators
+// Object stores more than one value
+let user = {
+  name: "leah",
+  age: 20,
+  class: "graduate",
+};
+let colors = ["red", "orange", "green"]; // array stores ordered data, and is classified under object
